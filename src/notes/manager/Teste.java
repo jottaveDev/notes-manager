1e17f462f3a1e1f8f889c74e31337d6f0568e2b6
felipe0402 <felipebaiano04@gmail.com>
2024-04-30 19:18:07 -0300

    teste

--- src/notes/manager/Teste.java
+++ src/notes/manager/Teste.java
@@ -11,7 +11,7 @@ public class Teste {
 
         Professor professor = new Professor("Pedro", "111111111", new ArrayList<>());
         Turma turma = new Turma("1 ano");
-        
+
         Aluno aluno1 = new Aluno("Joao", "00000000", "0000");
         Aluno aluno2 = new Aluno("Maria", "00000000", "0000");
         Aluno aluno3 = new Aluno("Ana", "00000000", "0000");
@@ -21,19 +21,20 @@ public class Teste {
         professor.adicionarAluno(turma, aluno1);
         professor.adicionarAluno(turma, aluno2);
         professor.adicionarAluno(turma, aluno3);
-        
+
         professor.getTurmas();
         turma.getAlunos();
-        
+
         professor.adicionarNota(aluno2, 4.0);
         professor.adicionarNota(aluno2, 2.5);
-        
+
         professor.adicionarNota(aluno3, 3.40);
         professor.adicionarNota(aluno3, 2.10);
         professor.adicionarNota(aluno3, 1.80);
-        
-//        turma.imprimirNotasAlunos();
+
+        // turma.imprimirNotasAlunos();
         turma.imprimirNotasAluno(aluno3);
+        turma.imprimirNotasAlunos();
     }
 
 }
